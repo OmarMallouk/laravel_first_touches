1d04ca87bb59c1c2241702e6c5eb870ec155c434
OmarMallouk <mr.mallouk.om@gmail.com>
2024-11-23 18:44:18 +0200

    adjusted the article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,7 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Article extends Model
-{
-    //
+class Article extends Model{
+    protected $table = 'articles';
+
+
+    protected $fillable = [
+        'title', 
+        'content', 
+        'author_id', 
+        'news_request_id', 
+    ];
+
+
+    public function author(){
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function newsRequest(){
+        return $this->belongsTo(News_Requests::class, 'news_request_id');
+    }
 }
