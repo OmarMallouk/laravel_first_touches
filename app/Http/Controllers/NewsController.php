263687951d81c54acc462d1e12a3291bdbcedae7
OmarMallouk <mr.mallouk.om@gmail.com>
2024-11-23 17:37:52 +0200

    added 2 more functions

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -20,6 +20,39 @@ class NewsController extends Controller{
             'age_limit' => 'nullable|integer',
             'attachment_path' => 'nullable|string|max:255'
         ]);
+
+        $news = News::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'author_id' => auth()->user()->id, 
+            'is_restricted' => $request->is_restricted ?? false,
+            'age_limit' => $request->age_limit,
+            'attachment_path' => $request->attachment_path,
+        ]);
+
+        return response()->json(['message' => 'News created successfully!', 'news' => $news], 201);
+    }
+    
+    function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'is_restricted' => 'boolean',
+            'age_limit' => 'nullable|integer',
+            'attachment_path' => 'nullable|string|max:255'
+        ]);
+
+        $news = News::findOrFail($id);
+        $news->update([
+            'title' => $request->title,
+            'content' => $request->content,
+            'is_restricted' => $request->is_restricted ?? false,
+            'age_limit' => $request->age_limit,
+            'attachment_path' => $request->attachment_path,
+        ]);
+
+        return response()->json(['message' => 'News updated successfully!', 'news' => $news]);
     }
 
 }
