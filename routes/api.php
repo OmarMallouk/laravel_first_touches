3d26de75f619863b0b24526a81f18e590cceccd5
OmarMallouk <mr.mallouk.om@gmail.com>
2024-11-23 19:21:14 +0200

    more fixes

--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\UserController;
-// use App\Http\Controllers\TestController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TestController;
 use App\Http\Controllers\NewsController;
 
 
-// Route::get('/users', [UserController::class, 'get_users']); 
+Route::get('/users', [UserController::class, 'get_users']); 
 
-Route::post('/news', [NewsController::class, 'create_news']);
+Route::middleware('admin')->group(function () {
+    Route::post('/news', [NewsController::class, 'store']);
+    Route::put('/news/{id}', [NewsController::class, 'update']);
+    Route::delete('/news/{id}', [NewsController::class, 'destroy']);
 
-// Route::middleware('admin')->group(function () {
-//     Route::put('/news/{id}', [NewsController::class, 'update']);
-//     Route::delete('/news/{id}', [NewsController::class, 'destroy']);
 
-
-//     Route::post('/articles', [ArticleController::class, 'store']);
-//     Route::put('/articles/{id}', [ArticleController::class, 'update']);
-//     Route::delete('/articles/{id}', [ArticleController::class, 'destroy']);
-// });
+    Route::post('/articles', [ArticleController::class, 'store']);
+    Route::put('/articles/{id}', [ArticleController::class, 'update']);
+    Route::delete('/articles/{id}', [ArticleController::class, 'destroy']);
+});
